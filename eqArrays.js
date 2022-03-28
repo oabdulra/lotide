edd6711c4d10afebf22617e82f40dd5549bd5632
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-03-28 19:43:13 -0400

    Adding a program to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+};
+
+const eqArrays = function(arrayOne , arrayTwo) {
+
+  for (let i = 0 ; i < arrayOne.length ; i++) {
+
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+
+  }
+
+  return true;
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
