459ba53c4d725941ac5582054341673d78b810bc
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-04-03 19:03:43 -0400

    first iteration of the eqObjects function

--- eqObjects.js
+++ eqObjects.js
@@ -8,5 +8,13 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
+
+  if (Object.keys(object1).length === Object.keys(object2).length){
+
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
