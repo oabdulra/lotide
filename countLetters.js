84b8a5b55352135e410302532a900d7a009ee8e1
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-04-03 11:35:34 -0400

    counting letters

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+};
+
+const countLetters = function (strToCount) {
+
+  let strToArray = strToCount.split("");
+  strToArray = strToCount.replace(/\s+/g, '');
+  
+
+  console.log(strToArray);
+  
+  const charCount = {};
+  
+  for (let letter = 0 ; letter<strToArray.length ; letter++){
+
+    
+    
+      if (typeof charCount[strToArray[letter]] !==  'undefined'){
+
+        charCount[strToArray[letter]] += 1;
+
+      }
+      else {
+        charCount[strToArray[letter]] = 1;
+      }
+    }
+
+  
+  
+  return charCount;
+};
+
+console.log(countLetters("lighthouse in the house"));
